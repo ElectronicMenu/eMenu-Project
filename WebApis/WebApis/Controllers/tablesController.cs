09ad9f41032b17a20d21693542848ba53bca082f
Vineet Dhammi <vineet.dts007@gmail.com>
2016-02-14 21:51:17 -0500

    Created a table API

---
+++ WebApis/WebApis/Controllers/tablesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApis;
+
+namespace WebApis.Controllers
+{
+    public class tablesController : ApiController
+    {
+        private hd_emenuEntities db = new hd_emenuEntities();
+
+        // GET: api/tables
+        public dynamic Gettables()
+        {
+            return new { table = db.tables };
+        }
+
+        // GET: api/tables/5
+        [ResponseType(typeof(table))]
+        public IHttpActionResult Gettable(int id)
+        {
+            table table = db.tables.Find(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(table);
+        }
+
+        // PUT: api/tables/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Puttable(int id, table table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != table.table_id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(table).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tableExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/tables
+        [ResponseType(typeof(table))]
+        public dynamic Posttable(table table)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            table tmptable = db.tables.SingleOrDefault(item => item.table_number == table.table_number && item.table_password == table.table_password);
+            if (tmptable == null)
+            {
+                return Ok(0);
+            }
+            return Ok(tmptable);
+        }
+
+        // DELETE: api/tables/5
+        [ResponseType(typeof(table))]
+        public IHttpActionResult Deletetable(int id)
+        {
+            table table = db.tables.Find(id);
+            if (table == null)
+            {
+                return NotFound();
+            }
+
+            db.tables.Remove(table);
+            db.SaveChanges();
+
+            return Ok(table);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool tableExists(int id)
+        {
+            return db.tables.Count(e => e.table_id == id) > 0;
+        }
+    }
+}
