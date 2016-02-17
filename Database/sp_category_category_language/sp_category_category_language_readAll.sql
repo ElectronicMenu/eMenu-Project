8b88652483a8e2ed3132f67432b98979f70205b6
Shobika <shobikaksr@gmail.com>
2016-02-17 16:14:37 -0500

    Created the stored procedures creation for table "category & category_language"
    Created the stored procedures creation for table "category &
    category_language":
    1. sp_category_category_language_deleteByID
    2. sp_category_category_language_insert
    3. sp_category_category_language_readByID
    4. sp_category_category_language_readAll

---
+++ Database/sp_category_category_language/sp_category_category_language_readAll.sql
@@ -0,0 +1,24 @@
+USE [hd_emenu]
+GO
+
+/****** Object:  StoredProcedure [dhammi].[sp_category_category_language_readAll]    Script Date: 2/17/2016 4:12:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dhammi].[sp_category_category_language_readAll]
+AS 
+BEGIN
+    SELECT a.category_id, a.category_image, a.category_icon, b.language_id, b.category_name
+    FROM [dbo].[category] a 
+	INNER JOIN [dbo].[category_language] b
+	ON a.category_id = b.category_id
+END
+
+GO
+
+
+
---
+++ Database/sp_category_category_language/sp_category_category_language_readByID.sql
@@ -0,0 +1,27 @@
+USE [hd_emenu]
+GO
+
+/****** Object:  StoredProcedure [dhammi].[sp_category_category_language_readByID]    Script Date: 2/17/2016 4:12:41 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+
+CREATE PROCEDURE [dhammi].[sp_category_category_language_readByID]
+	@category_id int
+AS 
+BEGIN
+    SELECT a.category_id, a.category_image, a.category_icon, b.language_id, b.category_name
+    FROM [dbo].[category] a 
+	INNER JOIN [dbo].[category_language] b
+	ON a.category_id = b.category_id
+	WHERE a.category_id =@category_id
+END
+
+
+GO
+
+
