ea0db17a0677d2d238617476e62cf3ba3051cd8b
Shobika <shobikaksr@gmail.com>
2016-03-13 15:17:05 -0400

    Updated and Removed Stored procedure cart and cart_product table
    Removed the stored proc for cart table with user because removed the
    user from the table and created new table for user and cart. Updated
    stored proc for cart_product table.

--- Database/sp_cart_product/sp_cart_product_product_readByCartIDAndLanguageID.sql
+++ Database/sp_cart_product/sp_cart_product_product_readByCartIDAndLanguageID.sql
@@ -1,7 +1,7 @@
 USE [hd_emenu]
 GO
 
-/****** Object:  StoredProcedure [hd_emenu].[sp_cart_product_product_readByCartID]    Script Date: 2016-02-20 9:35:38 PM ******/
+/****** Object:  StoredProcedure [hd_emenu].[sp_cart_product_product_readByCartIDAndLanguageID]    Script Date: 2016-03-13 3:09:54 PM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -9,8 +9,13 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 
-CREATE PROCEDURE [hd_emenu].[sp_cart_product_product_readByCartID]
-	@cart_id int
+
+
+
+
+CREATE PROCEDURE [hd_emenu].[sp_cart_product_product_readByCartIDAndLanguageID]
+	@cart_id int,
+	@language_id int
 AS 
 BEGIN
     SELECT c.product_name, c.product_description, b.product_image, b.product_is_veg, b.product_is_active, b. product_price,
@@ -20,8 +25,9 @@ BEGIN
 	ON b.product_id = a.product_id
 	INNER JOIN [dbo].[product_language] c
 	ON b.product_id = c.product_id
-	WHERE a.cart_id =@cart_id
+	WHERE a.cart_id =@cart_id AND C.language_id = @language_id
 END
+
 
 
 GO
